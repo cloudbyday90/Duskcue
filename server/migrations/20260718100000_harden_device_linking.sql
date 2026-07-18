e1036cbae1cebe05b242e79caa1658b8df81a530
Chris <cloudbyday90@outlook.com>
2026-07-18 17:12:53 -0400

    Fix copyright compliance headers

--- server/migrations/20260718100000_harden_device_linking.sql
+++ server/migrations/20260718100000_harden_device_linking.sql
@@ -1,5 +1,5 @@
 -- Duskcue — Self-hosted media streaming server
--- Copyright (C) 2026 Duskcue Contributors
+-- Copyright (C) 2026-2026 Duskcue Contributors
 --
 -- This program is free software: licensed under AGPL-3.0
 -- See LICENSE file for details.
